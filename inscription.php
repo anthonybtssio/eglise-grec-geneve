a9507bf4ef7c77d88e5e8cbe138ddeef636a0f6d
anthonybtssio <a.abouhable@gmail.com>
2026-04-21 20:49:01 +0200

    Update database credentials in inscription.php for secure configuration

--- inscription.php
+++ inscription.php
@@ -1,9 +1,9 @@
 <?php
 // --- CONFIGURATION O2SWITCH ---
 $host = "localhost";
-$dbname = "sc1zugo6376_aoas";   // <--- Ton vrai nom de base de données
-$user = "sc1zugo6376_anthony";  // <--- Ton vrai utilisateur MySQL
-$pass = "Antho2006";     // <--- Ton mot de passe est maintenant configuré !
+$dbname = "sc1zugo6376_aoas";   // <--- Ton vrai nom de base de données (sur o2switch, le préfixe est OBLIGATOIRE pour PHP)
+$user = "root";  // <--- Ton vrai utilisateur MySQL
+$pass = "";            // <--- Ton vrai mot de passe MySQL
 
 try {
     // Connexion sécurisée avec UTF-8
